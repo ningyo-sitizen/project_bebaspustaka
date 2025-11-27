87b510895a077136114facee73f77dce6744fc84
ningyo-sitizen <jowymone26@gmail.com>
2025-11-28 05:41:58 +0700

    new

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,12 @@ const path = require('path');
 const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const visitorcount = require('./routes/web')
+const infoCardRoutes = require('./routes/UserInfoRoutes')
 const verifyToken = require('./middleware/checktoken');
 const { getYearlyVisitors } = require('./controller/visitorcount');
 const {getDashboardDatVisitor} = require('./controller/visitorcount')
 const dashboardvisitorroutes = require('./routes/web');
+const { raw } = require('mysql2');
 require('dotenv').config({ path: __dirname + '/../.env' });
 
 const app = express();
@@ -18,18 +20,19 @@ app.use(cors({
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api/auth', authRoutes);
 
 app.get('/yearly_visitor', verifyToken, getYearlyVisitors);
 
-app.use('/api/dashboard', require('./routes/web')); // untuk dashboard
-app.use('/api', require('./routes/web')); // untuk ambil tahun doang
+app.use('/api/dashboard', require('./routes/web'));
+app.use('/api', require('./routes/web'));
 
-app.use('/api/loan',require('./routes/loanRouts'))
+app.use('/api/loan',require('./routes/loanRouts'));
 
 app.use('/api/landing',require('./routes/landing'));
+
+app.use('api/infoCard',require('./routes/infoCardRoutes'));
 
 
 app.listen(PORT, () => {
---
+++ backend/routes/infoCardRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const verifyToken = require('../middleware/checktoken');
+const { currentDayVisitorCount,getBorrowedBooksList,fetCurrentWeekVisitorVSLastWeek} = require('../controller/infocardcontroller');
+
+router.get('todayVisitorCount',verifyToken,currentDayVisitorCount);
+router.get('BorrowedBookList',verifyToken,getBorrowedBooksList);
+router.get('ThisWeekAndLast',verifyToken,fetCurrentWeekVisitorVSLastWeek);
