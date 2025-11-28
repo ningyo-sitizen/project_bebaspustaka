590e24753b0ae419a184f2735c15a75f9eff4791
zhrahprn <zahrah.purnama@gmail.com>
2025-11-28 09:30:30 +0700

    Resolve merge conflict in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,25 @@
 import { Routes, Route } from "react-router-dom";
+<<<<<<< Updated upstream
 import Login from "./Login.jsx";
 import Dashboard from "./Dashboard.jsx";
 import Analytic from "./Analytic.jsx";
 import Approval from "./Approval.jsx";
 import Profile from "./Profile.jsx";
 import EditProfile from "./Editprofile.jsx";
+=======
+import UserControl from "./Usercontrol";
+// import Login from "./Login.jsx";
+// import Dashboard from "./Dashboard.jsx";
+// import Analytic from "./Analytic.jsx";
+// import Profile from "./Profile";
+// import EditProfile from "./Editprofile";
+// import Sidebar from "./Sidebar";
+>>>>>>> Stashed changes
 
 export default function App() {
   return (
     <Routes>
+<<<<<<< Updated upstream
       <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />}/>
       <Route path="/dashboard" element={<Dashboard />} />
@@ -16,6 +27,18 @@ export default function App() {
       <Route path="/approval" element={<Approval />} />
       <Route path="/profile" element = {<Profile />}/>
       <Route path="/editprovile" element = {<EditProfile/>}/>
+=======
+      {/* /* <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />}/>
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/analytic" element={<Analytic />}></Route> */ }
+      {/* <Route path="/" element={<Profile />} />
+      <Route path="/" element={<EditProfile/>} /> */}
+      {/* <Route path="/" element={<Sidebar/>} /> */}
+      <Route path="/" element={<UserControl />} />
+    
+      
+>>>>>>> Stashed changes
     </Routes>
   );
 }
